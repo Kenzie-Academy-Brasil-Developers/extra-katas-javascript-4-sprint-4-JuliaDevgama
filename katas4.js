4b6c60c9e9bc5badf63fa654d667f158afe698a4
Julia Gama de Oliveira <86054348+JuliaDevgama@users.noreply.github.com>
2021-07-28 00:53:23 -0300

    a terminar

--- katas4.js
+++ katas4.js
@@ -6,8 +6,41 @@ const bestThing = "The best thing about a boolean is even if you are wrong you a
 
 function kata1() {
     // implemente aqui o código
+    let novoElemento = document.createElement('div')
+    novoElemento.textContent = JSON.stringify(gotCitiesCSV)
+    document.body.appendChild(novoElemento)
+    return gotCitiesCSV
 }
 
-// kata1();
+kata1();
 
-// daqui em diante é contigo
+function kata2() {
+    let novoElemento = document.createElement('div')
+    novoElemento.textContent = JSON.stringify(bestThing)
+    document.body.appendChild(novoElemento)
+
+    return bestThing
+}
+
+kata2()
+
+function kata3() {
+    let novoElemento = document.createElement('div')
+    // novoElemento.textContent = JSON.stringify(gotCitiesCSV)
+    // document.body.appendChild(novoElemento)
+    let matrix = gotCitiesCSV.split(' ')
+    console.log(matrix.join())
+   //return gotCitiesCSV
+}
+
+kata3()
+
+function kata4() {
+    let novoElemento = document.createElement('div')
+    novoElemento.textContent = JSON.stringify()
+    document.body.appendChild(novoElemento)
+
+   
+}
+
+kata4()
