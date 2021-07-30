25aa63920cb7a4c742bbec24d6bf969e3d5518c5
Julia Gama de Oliveira <86054348+JuliaDevgama@users.noreply.github.com>
2021-07-30 11:48:07 -0300

    a terminar ainda

--- katas4.js
+++ katas4.js
@@ -25,22 +25,71 @@ function kata2() {
 kata2()
 
 function kata3() {
+ 
     let novoElemento = document.createElement('div')
-    // novoElemento.textContent = JSON.stringify(gotCitiesCSV)
-    // document.body.appendChild(novoElemento)
-    let matrix = gotCitiesCSV.split(' ')
-    console.log(matrix.join())
-   //return gotCitiesCSV
+    novoElemento.textContent = JSON.stringify(gotCitiesCSV.replace(/,/g, ';'))
+    document.body.appendChild(novoElemento)
+
+    return gotCitiesCSV
 }
 
 kata3()
 
 function kata4() {
     let novoElemento = document.createElement('div')
-    novoElemento.textContent = JSON.stringify()
+    novoElemento.textContent = JSON.stringify(gotCitiesCSV.split(' '))
     document.body.appendChild(novoElemento)
 
-   
+    return gotCitiesCSV
 }
 
 kata4()
+
+function kata5() {
+    /* Escreva uma função que **retorna** um array com as 5 primeiras cidades de 'lotrCitiesArray'. Lembre-se de também **adicionar os resultados à página*.*/
+    let novoElemento = document.createElement('div')
+    let nova = []
+        for(let i = 0; i < lotrCitiesArray.length-3; i++){
+        nova.push(lotrCitiesArray[i])
+    }
+    novoElemento.textContent = JSON.stringify(nova)
+    document.body.appendChild(novoElemento)
+
+    return lotrCitiesArray
+}
+
+kata5()
+
+function kata6() {
+   
+    let novoElemento = document.createElement('div')
+    let novaDenovo = []
+
+    for(let i = 3; i < lotrCitiesArray.length; i++){
+        novaDenovo.push(lotrCitiesArray[i])
+    }
+    novoElemento.textContent = JSON.stringify(novaDenovo)
+    document.body.appendChild(novoElemento)
+
+    return lotrCitiesArray
+}
+
+kata6()
+
+function kata7() {
+    let novoElemento = document.createElement('div')
+    let novaSouEuDenovo = []
+
+    for(let i = 2; i < lotrCitiesArray.length-3;i++){
+        novaSouEuDenovo.push(lotrCitiesArray[i])
+    }
+
+    novoElemento.textContent = JSON.stringify(novaSouEuDenovo)
+    document.body.appendChild(novoElemento)
+
+    return lotrCitiesArray
+}
+
+kata7()
+
+
